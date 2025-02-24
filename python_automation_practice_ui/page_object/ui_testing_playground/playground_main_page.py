7d5313029f0dd86b3a50684ec099db07db2f794a
Trinh Phat <trinhphat001@gmail.com>
2025-02-23 19:00:57 -0500

    Remove web driver and add unit test for widget and main page object

--- python_automation_practice_ui/page_object/ui_testing_playground/playground_main_page.py
+++ python_automation_practice_ui/page_object/ui_testing_playground/playground_main_page.py
@@ -1,6 +1,5 @@
 from python_automation_practice_ui.page_object.web_locator.playground_locator import PlayGroundUrl, PlayGroundId, \
     PlayGroundXpath
-from python_automation_practice_ui.web_driver.web_driver import WebDriver
 from selenium.webdriver.common.by import By
 
 
@@ -41,19 +40,3 @@ class PlaygroundMainPage:
 
     def description_text(self):
         return self.description_element().text
-
-
-if __name__ == '__main__':
-    dict_options = {
-        "timeouts.pageLoad": 5000,
-        "timeouts.implicit": 5000,
-        "max_windows_size": True
-    }
-    driver = WebDriver("chrome", dict_options).driver
-    page_playground = PlaygroundMainPage(driver)
-    page_playground.open()
-    print(page_playground.title_text())
-    print(page_playground.block_quote_text())
-    print(page_playground.block_quote_footer_text())
-    print(page_playground.block_quote_alert_text())
-    print(page_playground.description_text())
